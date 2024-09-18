cfd04e1ed4d915b424a901709f15871bfce62d4c
Ahsan Habib <arman0sheikh@gmail.com>
2024-09-18 15:36:50 +0600

    logo font changed for mobile

--- app/components/client/MenuBar.jsx
+++ app/components/client/MenuBar.jsx
@@ -7,7 +7,7 @@ const MenuBar = () => {
     <div className="search_bar sticky top-0 z-50 h-auto w-full border-b bg-white">
       <div className="container mx-auto flex items-center justify-between gap-3 px-2 py-3 sm:gap-8 sm:px-4 sm:py-4 md:px-8 lg:gap-16">
         <Link href={"/"} className="logo flex items-center gap-2 sm:text-xl">
-          <h3 className="text-lg font-bold sm:text-2xl">
+          <h3 className="text-xl font-bold sm:text-2xl">
             <span className="text-orange-500">ইশফাবিডি</span>.কম
           </h3>
         </Link>
